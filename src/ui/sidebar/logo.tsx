640528a0e1725419774fc53050fceddd3187ed6b
Sergey Bashkin <se.bashkin@ya.ru>
2022-01-01 11:24:37 +0400

    fix: fixed showing image om site

--- src/ui/sidebar/logo.tsx
+++ src/ui/sidebar/logo.tsx
@@ -1,5 +1,6 @@
 import { Typography } from "@mui/material";
 import styled from "styled-components";
+import logoImg from "../../assets/logo.png";
 
 const Root = styled("div")`
   padding-top: 50px;
@@ -15,7 +16,7 @@ const Image = styled("img")`
 export const Logo = () => {
   return (
     <Root>
-      <Image src="/src/assets/logo.png" />
+      <Image src={logoImg} />
       <Typography variant="h1" fontSize={20}>
         Сборники Христиан
       </Typography>
